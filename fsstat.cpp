81da35beac1c60834245e4445284ec4f1f2dde6a
Ian Brault <ian.brault@engineering.ucla.edu>
2018-05-22 18:00:03 -0700

    Print out filesystem usage info.

--- fsstat.cpp
+++ fsstat.cpp
@@ -11,6 +11,8 @@
 #include <cstdlib>
 #include <iomanip>
 #include <iostream>
+#include <sstream>
+#include <string>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -58,10 +60,53 @@ split_ws(const std::string& s)
     split(s, ' ', v1);
 
     // filter empty elements
-    auto filter = [](std::string s){ return s != ""; };
-    auto it = std::copy_if(v1.begin(), v1.end(), v2.begin(), filter);
-    v2.resize(std::distance(v2.begin(), it));
+    auto filter = [](const std::string& s){ return s != ""; };
+    auto it = std::copy_if(v1.begin(), v1.end(), std::back_inserter(v2), filter);
     return v2;
+}
+
+
+void
+print_fsrow(const std::string& col)
+{
+    // split info into columns
+    std::vector<std::string> cols = split_ws(col);
+    // print filesystem
+    std::cout << "  " << std::setw(14) << std::left << cols[0];
+    
+    // convert values (all are in sizes of 512-byte blocks)
+    long long size, use, avail;
+    size  = std::stoll(cols[1]) * 512;
+    use   = std::stoll(cols[2]) * 512;
+    avail = std::stoll(cols[3]) * 512;
+
+    // convert disk usage to readable units
+    for (const auto& unit : { size, use, avail })
+    {
+        double unitf;
+        std::ostringstream unitstr;
+        if (unit >= 1.0e9) {
+            unitf = unit / 1.0e9;
+            unitstr << std::setprecision(4) << unitf << "GB";
+        } else if (unit >= 1.0e6) {
+            unitf = unit / 1.0e6;
+            unitstr << std::setprecision(4) << unitf << "MB";
+        } else {
+            unitf = unit / 1.0e3;
+            unitstr << std::setprecision(4) << unitf << "kB";
+        }
+        std::cout << std::setw(10) << std::right << unitstr.str();
+    }
+
+    // display disk usage percentage (cutoff at 1.0e-3 precision)
+    std::ostringstream diskstr;
+    double disk = ((1.0 * use) / size) * 100;
+    if (disk < 1.0e-3) disk = 0;
+    diskstr << std::setprecision(3) << disk << "%";
+    std::cout << std::setw(10) << std::right << diskstr.str();
+
+    // display mount point
+    std::cout << std::setw(9) << std::right << cols[8] << std::endl;
 }
 
 
@@ -72,18 +117,13 @@ print_fsinfo(const std::string& dfout)
     std::vector<std::string> lines;
     split(dfout, '\n', lines);
 
-    // print first column
-    std::string fs = "Filesystem";
-    std::cout << std::setw(fs.length() + 2) << std::left << fs;
-    std::cout << std::setw(8) << std::right << "Size";
-    std::cout << std::setw(8) << std::right << "Used";
-    std::cout << std::setw(8) << std::right << "Avail";
-    std::cout << std::setw(8) << std::right << "Use%";
-    std::cout << std::setw(8) << std::right << "iUse%";
-    std::cout << std::setw(8) << std::right << "Mount\n";
+    // print first row
+    std::cout << std::setw(16) << std::left << "Filesystem";
+    for (const auto& x : { "Size", "Used", "Avail", "Used", "Mount\n" })
+        std::cout << std::setw(10) << std::right << x;
 
-    std::vector<std::string> cols = split_ws(lines[1]);
-    for (const auto& c : cols) std::cout << c << std::endl;
+    // print each filesystem row(s)
+    std::for_each(lines.begin() + 1, lines.end(), print_fsrow);
 }
 
 
